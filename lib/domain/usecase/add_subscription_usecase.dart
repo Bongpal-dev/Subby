76613dead2fa09369b692890522fab239c015368
Bongpal-dev <etgt777@naver.com>
2026-01-10 14:22:09 +0900

    feat: AddSubscriptionUseCase Firebase 동기화
    - PendingChangeRepository 주입
    - FirebaseSyncException 발생 시 PendingChange 저장
    
    Closes #103

--- lib/domain/usecase/add_subscription_usecase.dart
+++ lib/domain/usecase/add_subscription_usecase.dart
@@ -1,10 +1,23 @@
+import 'package:subby/core/error/firebase_sync_exception.dart';
+import 'package:subby/domain/model/pending_change.dart';
 import 'package:subby/domain/model/user_subscription.dart';
+import 'package:subby/domain/repository/pending_change_repository.dart';
 import 'package:subby/domain/repository/subscription_repository.dart';
 
 class AddSubscriptionUseCase {
   final SubscriptionRepository _repository;
+  final PendingChangeRepository _pendingChangeRepository;
 
-  AddSubscriptionUseCase(this._repository);
+  AddSubscriptionUseCase(this._repository, this._pendingChangeRepository);
 
-  Future<void> call(UserSubscription subscription) => _repository.create(subscription);
+  Future<void> call(UserSubscription subscription) async {
+    try {
+      await _repository.create(subscription);
+    } on FirebaseSyncException {
+      await _pendingChangeRepository.saveSubscriptionChange(
+        subscription,
+        ChangeAction.create,
+      );
+    }
+  }
 }
