a5bb8f7762e2dff8439b5de0a305794ca9f1f64f
Bongpal-dev <etgt777@naver.com>
2026-01-12 13:25:26 +0900

    feat: 실시간 동기화 서비스 구현
    - RealtimeSyncService 생성
    - Firebase 변경 감지 시 Local DB 자동 동기화
    - 추가/수정/삭제 동기화 로직 구현
    
    Closes #39
    Closes #40
    Closes #41

--- lib/core/di/providers.dart
+++ lib/core/di/providers.dart
@@ -1,4 +1,5 @@
 export 'database_provider.dart';
 export 'data/datasource_providers.dart';
+export 'data/service_providers.dart';
 export 'domain/repository_providers.dart';
 export 'domain/usecase_providers.dart';
