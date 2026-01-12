944be67743c2cae41cedc121ccf9a6320eccc39e
Bongpal-dev <etgt777@naver.com>
2026-01-12 17:58:40 +0900

    feat: HTTPS 초대 링크 구현
    - Firebase Hosting 리다이렉트 페이지 추가
    - InviteLinkGenerator가 https 링크 생성하도록 변경
    - 카카오톡 등에서 클릭 가능한 링크로 공유

--- lib/core/util/invite_link_generator.dart
+++ lib/core/util/invite_link_generator.dart
@@ -2,16 +2,30 @@ class InviteLinkGenerator {
   static const _scheme = 'subby';
   static const _host = 'join';
   static const _codeParam = 'code';
+  static const _webHost = 'subby-91b88.web.app';
 
+  /// 공유용 HTTPS 링크 생성
   static String generate(String groupCode) {
+    return 'https://$_webHost/join?$_codeParam=$groupCode';
+  }
+
+  /// 앱 내부용 딥링크 생성
+  static String generateDeepLink(String groupCode) {
     return '$_scheme://$_host?$_codeParam=$groupCode';
   }
 
+  /// 딥링크에서 그룹 코드 파싱
   static String? parseGroupCode(Uri uri) {
-    if (uri.scheme != _scheme || uri.host != _host) {
-      return null;
+    // subby:// 스킴
+    if (uri.scheme == _scheme && uri.host == _host) {
+      return uri.queryParameters[_codeParam];
     }
 
-    return uri.queryParameters[_codeParam];
+    // https:// 웹 링크
+    if (uri.scheme == 'https' && uri.host == _webHost && uri.path == '/join') {
+      return uri.queryParameters[_codeParam];
+    }
+
+    return null;
   }
 }
