5908a4d4b934719fc8b0b40797c5ea9b798370bc
Bongpal-dev <etgt777@naver.com>
2026-01-28 20:37:04 +0900

    feat: AppFab 컴포넌트 추가
    - Accent 색상 배경의 FAB 구현
    - 커스텀 아이콘 지원
    - 라이트/다크 모드 대응

---
+++ lib/presentation/common/widgets/app_fab.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:subby/core/theme/app_colors.dart';
+import 'package:subby/core/theme/app_radius.dart';
+import 'package:subby/core/theme/app_icons.dart';
+
+/// Figma 디자인 시스템 FAB (Floating Action Button)
+class AppFab extends StatelessWidget {
+  const AppFab({
+    super.key,
+    required this.onPressed,
+    this.icon = AppIconType.plus,
+  });
+
+  final VoidCallback onPressed;
+  final AppIconType icon;
+
+  @override
+  Widget build(BuildContext context) {
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+    final colors = isDark ? AppColors.dark : AppColors.light;
+
+    return Material(
+      color: colors.bgAccent,
+      borderRadius: AppRadius.lgAll,
+      elevation: 4,
+      shadowColor: Colors.black26,
+      child: InkWell(
+        onTap: onPressed,
+        borderRadius: AppRadius.lgAll,
+        child: Container(
+          width: 56,
+          height: 56,
+          alignment: Alignment.center,
+          child: AppIcon(
+            icon,
+            size: 24,
+            color: colors.iconOnAccent,
+          ),
+        ),
+      ),
+    );
+  }
+}
